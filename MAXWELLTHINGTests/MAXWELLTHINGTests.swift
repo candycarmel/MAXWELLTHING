91f09ed6b3eb42c0690ac651238ae33da70a2c69
matthew fitch <matthew.fitch26@d155.org>
2025-02-13 05:31:10 -0800

    Initial Commit

---
+++ MAXWELLTHINGTests/MAXWELLTHINGTests.swift
@@ -0,0 +1,17 @@
+//
+//  MAXWELLTHINGTests.swift
+//  MAXWELLTHINGTests
+//
+//  Created by MATTHEW FITCH on 2/13/25.
+//
+
+import Testing
+@testable import MAXWELLTHING
+
+struct MAXWELLTHINGTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
